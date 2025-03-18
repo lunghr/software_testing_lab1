c13671e0a168dea1d40391f9921a7d4849debe3e
lunghr <myilanka8@gmail.com>
2025-03-18 13:53:01 +0300

    feat: blank project

---
+++ src/main/kotlin/com/example/software_testing_lab1/SoftwareTestingLab1Application.kt
@@ -0,0 +1,11 @@
+package com.example.software_testing_lab1
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SoftwareTestingLab1Application
+
+fun main(args: Array<String>) {
+    runApplication<SoftwareTestingLab1Application>(*args)
+}
---
+++ src/test/kotlin/com/example/software_testing_lab1/SoftwareTestingLab1ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.software_testing_lab1
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SoftwareTestingLab1ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
